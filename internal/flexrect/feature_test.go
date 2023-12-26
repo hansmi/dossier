6cb7315cb77ea205cf6816bc38dea086686faf62
Michael Hanselmann <public@hansmi.ch>
2023-12-26 23:08:27 +0100

    Add test for node feature comparison

--- internal/flexrect/feature_test.go
+++ internal/flexrect/feature_test.go
@@ -1,12 +1,15 @@
 package flexrect
 
 import (
+	"math/rand"
+	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/hansmi/dossier/internal/sketcherror"
 	"github.com/hansmi/dossier/proto/sketchpb"
+	"golang.org/x/exp/slices"
 )
 
 func TestNodeFeature(t *testing.T) {
@@ -51,6 +54,36 @@ func TestNodeFeature(t *testing.T) {
 
 			if gotCompare := got.compare(got); gotCompare != 0 {
 				t.Errorf("compare() against itself returned %d, want 0", gotCompare)
+			}
+		})
+	}
+}
+
+func TestNodeFeatureCompare(t *testing.T) {
+	items := []NodeFeature{
+		{},
+		{name: "a", feature: sketchpb.NodeFeature_BOTTOM_RIGHT},
+		{name: "b"},
+		{name: "b", feature: sketchpb.NodeFeature_BOTTOM_LEFT},
+		{name: "b", feature: sketchpb.NodeFeature_BOTTOM_RIGHT},
+		{name: "c", feature: sketchpb.NodeFeature_TOP_LEFT},
+		{name: "c", feature: sketchpb.NodeFeature_TOP_RIGHT},
+	}
+
+	for i := 0; i < (2 * len(items)); i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			tmp := slices.Clone(items)
+
+			rand.Shuffle(len(tmp), func(a, b int) {
+				tmp[a], tmp[b] = tmp[b], tmp[a]
+			})
+
+			slices.SortStableFunc(tmp, func(a, b NodeFeature) int {
+				return a.compare(b)
+			})
+
+			if diff := cmp.Diff(items, tmp, cmp.AllowUnexported(NodeFeature{})); diff != "" {
+				t.Errorf("compare() diff (-want +got):\n%s", diff)
 			}
 		})
 	}
