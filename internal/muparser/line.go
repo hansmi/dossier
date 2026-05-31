78a00abb67ed119b766e1b533fe2300d99a6e1b9
Michael Hanselmann <public@hansmi.ch>
2026-05-31 22:14:11 +0200

    RangeBounds receives bounds by bytes, not runes
    The previous implementation incorrectly assumed that the given indices
    are for bytes. That works as long as only characters encoded by a single
    UTF-8 byte are used.

--- internal/muparser/line.go
+++ internal/muparser/line.go
@@ -1,7 +1,9 @@
 package muparser
 
 import (
+	"fmt"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/hansmi/dossier/internal/mutool/stext"
 	"github.com/hansmi/dossier/internal/ref"
@@ -54,11 +56,69 @@ func (l *Line) Text() string {
 	return *l.text
 }
 
+// RangeBounds returns the rectangular bounds enclosing the characters between
+// the byte offsets start and end.
 func (l *Line) RangeBounds(start, end int) geometry.Rect {
-	chars := l.chars[start:end]
+	raiseOutOfRange := func() {
+		panic(fmt.Errorf("bounds [%d:%d] out of range", start, end))
+	}
+
+	if start < 0 || end < start {
+		raiseOutOfRange()
+	}
+
+	runeStart := -1
+	runeEnd := -1
+	offset := 0
+
+	// Map byte offsets to rune indices.
+	for idx, c := range l.chars {
+		size := utf8.RuneLen(c.C)
+
+		// Find the starting boundary.
+		if runeStart < 0 && start < offset+size {
+			runeStart = idx
+		}
+
+		// Find the ending boundary.
+		if runeEnd < 0 && end <= offset+size {
+			if end == offset {
+				runeEnd = idx
+			} else {
+				runeEnd = idx + 1
+			}
+		}
+
+		offset += size
+
+		if runeStart >= 0 && runeEnd >= 0 {
+			// Both ends have been found.
+			break
+		}
+	}
+
+	if runeStart < 0 {
+		// Happens if start equals the total byte length. In Go, slicing
+		// exactly at the end (e.g., slice[len:len]) safely returns an empty
+		// slice.
+		runeStart = len(l.chars)
+	}
+
+	// Enforce the upper length boundary.
+	if runeEnd < 0 {
+		// The requested end exceeded the total bytes. The only valid exception
+		// is an empty slice where end == 0.
+		if len(l.chars) == 0 && end == 0 {
+			runeEnd = 0
+		} else {
+			raiseOutOfRange()
+		}
+	}
+
+	chars := l.chars[runeStart:runeEnd]
 	bounds := chars[0].Bounds
 
-	if len(chars) > 1 {
+	if len(chars) > 0 {
 		for _, c := range chars[1:] {
 			bounds = bounds.Union(c.Bounds)
 		}
